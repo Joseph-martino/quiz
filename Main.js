5ff931ce90270469eee873a9d3eb1b4f995fb5d7
Joseph-martino <71452904+Joseph-martino@users.noreply.github.com>
2023-07-24 10:36:33 +0200

    enter button can be use instead od valider button

--- Main.js
+++ Main.js
@@ -2,6 +2,12 @@
 
 StartButton.addEventListener('click',launchGame);
 validationbutton.addEventListener('click', checkUserAnswer);
+userInput.addEventListener('keypress', function(event){
+    if(event.key === "Enter"){
+        event.preventDefault();
+        validationbutton.click();
+    }
+});
 skipButton.addEventListener('click', skip);
 clueButton.addEventListener('click', showClue);
 restartButton.addEventListener('click', reloadGame);

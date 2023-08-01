49b70058e33f25c7f05c6d1fec05ac8aa04f85bc
Joseph-martino <71452904+Joseph-martino@users.noreply.github.com>
2023-08-01 23:25:25 +0200

    timer changes in progress

--- services/TimerService.js
+++ services/TimerService.js
@@ -3,6 +3,9 @@ class TimerService {
     rewardService = new RewardService();
     scoreService = new ScoreService();
     gameOverService = new GameOverService();
+    timerDelay;
+    test = 30;
+    interval;
 
     initializeNumberOfSecond(){
         numberOfSeconds = 30;
@@ -16,17 +19,45 @@ class TimerService {
         let currentNumbrOfSeconds = parseInt(timerDisplay.innerText);
         return currentNumbrOfSeconds;
     }
+
+    changeTimer(number){
+        this.timerDelay = number;
+    }
+
+    addContentToTimer(){
+        numberOfSeconds--;
+        this.displayTimer(numberOfSeconds);
+    }
     
     timer(){
-        const timer = window.setInterval(() => {
-        numberOfSeconds--
-        if(numberOfSeconds < 0){
-            this.gameOverService.setEndgame(this.gameOver);
-            numberOfSeconds = 0;
-            this.stopTimer(timer);
+        clearInterval(this.interval);
+        this.addContentToTimer();
+        let score = this.scoreService.getCurrentScore();
+        if(score >= 0 && score <= 20){
+            console.log("lent");
+            this.changeTimer(1500);
+        } else if(score >20 && score <= 40){
+            console.log("moyen");
+            this.changeTimer(1000);
+        } else if(score > 40){
+            console.log("rapide");
+            this.changeTimer(700);
         }
-        this.displayTimer(numberOfSeconds)} 
-        , timerInterval);
+        this.interval = window.setInterval(this.timer, this.timerDelay);
+        if(numberOfSeconds <= 0){
+            numberOfSeconds = 0;
+            clearInterval(this.interval);
+        }
+
+        // const timer = window.setInterval(() => {
+        // numberOfSeconds--
+        // if(numberOfSeconds < 0){
+        //     this.gameOverService.setEndgame(this.gameOver);
+        //     numberOfSeconds = 0;
+        //     this.stopTimer(timer);
+        // }
+        // this.displayTimer(numberOfSeconds)} 
+        // , timerInterval);
     }
     
     stopTimer(timer) {
